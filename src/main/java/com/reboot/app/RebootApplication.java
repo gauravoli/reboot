9284d37c1d719e4a1ebd1386283d9f9aa1a742d1
Gaurav Oli <gauravoli85@gmail.com>
2017-10-07 13:34:22 -0400

    Initial Commmit - Project Setup

---
+++ src/main/java/com/reboot/app/RebootApplication.java
@@ -0,0 +1,12 @@
+package com.reboot.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RebootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RebootApplication.class, args);
+	}
+}
